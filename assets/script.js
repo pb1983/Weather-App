7f26a7564e1b9d5d2b80a45cac44699df04e9257
pb1983 <phil.bryer@gmail.com>
2023-08-05 14:49:39 -0400

    isolated appropriate data points from fetch. added imperial unit parameter for temperature. addad daysjs and converted from unix time.

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,38 @@ let weatherLoc = document.querySelector("#location");
 let submit = document.querySelector("#submit");
 
 
-fetch("api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid=")
+submit.addEventListener("click", function(event) {
+
+event.target.matches("button");
+
+let location = weatherLoc.value;
+
+
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=0dffd273a14a316064e1b544e7c8e115`)
     .then(response => response.json())
     .then(cities => {
         let firstCity = cities[0];
-        console.log(firstCity.lat);
-        console.log(firstCity.lon);
-        
-        return fetch("api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key")
+            
+        return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&units=imperial&appid=0dffd273a14a316064e1b544e7c8e115`)
     })
 
     .then(response => response.json())
-    .then(data => {
+    .then(weatherInfo => {
 
-        console.log(data)
+       
+        let realDate = dayjs.unix(weatherInfo.list[0].dt).format("MM/DD/YYYY hh:mm:ss");
+
+        console.log(weatherInfo)
+        console.log(realDate)
+        console.log(weatherInfo.list[0].main.humidity)
+        console.log(weatherInfo.list[0].main.temp)
+        console.log(weatherInfo.list[0].wind.speed)
+        console.log(weatherInfo.list[0].weather[0].icon)
+    
     })
+
+})
+
+
+    //dayjs.unix(1691668800).format("MM/DD/YYYY hh:mm:ss") to get time stamps for each day. 
+    // city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed. 
