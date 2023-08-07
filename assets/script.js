86283088cba78382f109dac4809cf52d8ab2c7cf
pb1983 <phil.bryer@gmail.com>
2023-08-06 23:42:32 -0400

    Added text content to event listener for current day forcast. Added css styling to put input and buttons in a column. Provided background color to header. Linked weather icons to return data.

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,56 @@
 let weatherLoc = document.querySelector("#location");
 let submit = document.querySelector("#submit");
+let cityNow = document.querySelector("#currentCity");
+let dateNow = document.querySelector("#currentDate");
+let tempNow = document.querySelector("#currentTemp")
+let humidityNow = document.querySelector("#currentHum");
+let windNow = document.querySelector("#currentWind");
+let firstCity = "";
+let iconLink = document.querySelector("img");
 
 
-submit.addEventListener("click", function(event) {
+submit.addEventListener("click", function (event) {
 
-event.target.matches("button");
+    event.target.matches("submit");
 
-let location = weatherLoc.value;
+    let location = weatherLoc.value;
 
 
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=0dffd273a14a316064e1b544e7c8e115`)
-    .then(response => response.json())
-    .then(cities => {
-        let firstCity = cities[0];
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=0dffd273a14a316064e1b544e7c8e115`)
+        .then(response => response.json())
+        .then(cities => {
+            let firstCity = cities[0];
+            cityNow.textContent = firstCity.name;
+
+            return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&units=imperial&appid=0dffd273a14a316064e1b544e7c8e115`)
+
+
+
+        })
+
+        .then(response => response.json())
+        .then(weatherInfo => {
+
             
-        return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&units=imperial&appid=0dffd273a14a316064e1b544e7c8e115`)
-    })
+            let realDate = dayjs.unix(weatherInfo.list[0].dt).format("MM/DD/YYYY");
+            console.log(weatherInfo);
+            dateNow.textContent = realDate;
+            humidityNow.textContent = "Humidity: " + weatherInfo.list[0].main.humidity + " %"
+            tempNow.textContent = "Temp: " + weatherInfo.list[0].main.temp + "\u00B0F";
+            windNow.textContent = " Wind " + weatherInfo.list[0].wind.speed + " MPH";
 
-    .then(response => response.json())
-    .then(weatherInfo => {
+            let icon = weatherInfo.list[0].weather[0].icon;
 
-       
-        let realDate = dayjs.unix(weatherInfo.list[0].dt).format("MM/DD/YYYY hh:mm:ss");
+            iconLink.src = `https://openweathermap.org/img/wn/${icon}@2x.png`
 
-        console.log(weatherInfo)
-        console.log(realDate)
-        console.log(weatherInfo.list[0].main.humidity)
-        console.log(weatherInfo.list[0].main.temp)
-        console.log(weatherInfo.list[0].wind.speed)
-        console.log(weatherInfo.list[0].weather[0].icon)
-    
-    })
+
+            
+
+        })
+
 
 })
 
 
-    //dayjs.unix(1691668800).format("MM/DD/YYYY hh:mm:ss") to get time stamps for each day. 
-    // city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed. 
+
+
