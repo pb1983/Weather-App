c3033a4efed996198aa470ba8f3f54e89c4d2ffb
pb1983 <phil.bryer@gmail.com>
2023-08-07 23:54:08 -0400

    Added loop to append elements to 5 day forecast. Updated CSS styling to 5 day forecast. Added Readme file. Modified HTML to keep sections organized.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Weather-App
+
+### This application provides the current weather in a city provided by the user. The query will return the location, date, temperature, wind speed, and humidity for the current date as well as a 5 day forcast. There is also an icon that will be displayed for each day that corresponds with the expected conditions. A history of the areas searched by the user are included on the page with clickable links that will redisplay that city's forecast. Methods used to complete the assignment include: 
+
+
+* Use of local storage to keep search history on the page. 
+
+* For loops to compile different types of data into a single display. 
+
+* Retrieving information from APIs via the fetch method.
+
+* Appending elements with javascript rather than static HTML.
+
+* Dayjs API for converting unix time to date time. 
+
+* Bootstrap for CSS styling.
+
+
+
+
+[URL] https://pb1983.github.io/Weather-App/
--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,20 @@
 let weatherLoc = document.querySelector("#location");
 let submit = document.querySelector("#submit");
-let cityNow = document.querySelector("#currentCity");
-let dateNow = document.querySelector("#currentDate");
-let tempNow = document.querySelector("#currentTemp")
-let humidityNow = document.querySelector("#currentHum");
-let windNow = document.querySelector("#currentWind");
+let city = document.querySelector("#currentCity");
+let date = document.querySelector("#currentDate");
+let temp = document.querySelector("#currentTemp")
+let humidity = document.querySelector("#currentHum");
+let wind = document.querySelector("#currentWind");
 let firstCity = "";
 let iconLink = document.querySelector("img");
+let realDate = "";
+let icon = "";
+let fiveDayForcast = document.querySelector("#forecast");
+let cityBtn = document.querySelector("#cityList");
+let newBtn = document.createElement("button")
+let citiesSearched = localStorage.getItem || [];
+
+
 
 
 submit.addEventListener("click", function (event) {
@@ -20,7 +28,7 @@ submit.addEventListener("click", function (event) {
         .then(response => response.json())
         .then(cities => {
             let firstCity = cities[0];
-            cityNow.textContent = firstCity.name;
+            city.textContent = firstCity.name;
 
             return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&units=imperial&appid=0dffd273a14a316064e1b544e7c8e115`)
 
@@ -29,27 +37,79 @@ submit.addEventListener("click", function (event) {
         })
 
         .then(response => response.json())
-        .then(weatherInfo => {
+        .then(response => {
+            console.log(response.list)
+        
+            let weatherInfo = response.list
 
-            
-            let realDate = dayjs.unix(weatherInfo.list[0].dt).format("MM/DD/YYYY");
             console.log(weatherInfo);
-            dateNow.textContent = realDate;
-            humidityNow.textContent = "Humidity: " + weatherInfo.list[0].main.humidity + " %"
-            tempNow.textContent = "Temp: " + weatherInfo.list[0].main.temp + "\u00B0F";
-            windNow.textContent = " Wind " + weatherInfo.list[0].wind.speed + " MPH";
+            
+            realDate = dayjs.unix(weatherInfo[0].dt).format("MM/DD/YYYY");
+            date.textContent = realDate;
+            humidity.textContent = "Humidity: " + weatherInfo[0].main.humidity + " %"
+            temp.textContent = "Temp: " + weatherInfo[0].main.temp + "\u00B0F";
+            wind.textContent = " Wind: " + weatherInfo[0].wind.speed + " MPH";
 
-            let icon = weatherInfo.list[0].weather[0].icon;
+            icon = weatherInfo[0].weather[0].icon;
 
             iconLink.src = `https://openweathermap.org/img/wn/${icon}@2x.png`
 
-
+            weatherInfo = response.list;
             
 
+                for (let i = 0; i < weatherInfo.length; i+=8) {
+                    
+
+                    let containerDiv = document.createElement("div");
+                    let contentDiv = document.createElement("div");
+                    let fiveDayDate = document.createElement("h5");
+                    let fiveDayIcon = document.createElement("img");
+                    let fiveDayTemp = document.createElement("p");
+                    let fiveDayWind = document.createElement("p");
+                    let fiveDayHum = document.createElement("p");
+
+                    
+                    containerDiv.classList.add("card", "card-width", "row", "col-9");
+                    containerDiv.classList.add("card-body");
+                    fiveDayDate.classList.add("card-title");            
+                    fiveDayTemp.classList.add("card-text")         
+                    fiveDayWind.classList.add("card-text")
+                    fiveDayHum.classList.add("card-text")
+
+                    fiveDayForcast.appendChild(containerDiv)
+                    containerDiv.appendChild(contentDiv);
+                    contentDiv.appendChild(fiveDayDate);
+                    contentDiv.appendChild(fiveDayTemp);
+                    contentDiv.appendChild(fiveDayWind);
+                    contentDiv.appendChild(fiveDayHum);
+                    contentDiv.appendChild(fiveDayIcon);
+                  
+               
+                    fiveDayDate.textContent = dayjs.unix(weatherInfo[i].dt).format("MM/DD/YYYY");
+                    fiveDayTemp.textContent = "Temp: " + weatherInfo[i].main.temp + "\u00B0F";
+                    fiveDayWind.textContent = " Wind: " + weatherInfo[i].wind.speed + " MPH";
+                    fiveDayHum.textContent = "Humidity: " + weatherInfo[i].main.humidity + " %";
+
+                    icon = weatherInfo[i].weather[0].icon;
+
+                    fiveDayIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`
+
+                   console.log(fiveDayWind, fiveDayHum, fiveDayTemp)
+                  
+               
+                  
+                }
         })
+   })
 
+    
+ function cityHistory(event) {
+    if (event.target.matches("submit")) {
 
-})
+        localStorage.setItem(citiesSearched, weatherLoc.value);
+    }
+       
+ }
 
 
 
